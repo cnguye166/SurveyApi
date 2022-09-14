ed514473ca1544f177b15a27b90b0430d7ab54f1
Charles Nguyen <www.CharlesNguyen5@gmail.com>
2022-09-14 13:15:43 -0500

    Model In Progress

---
+++ Entities/Models/Survey.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Entities.Models
+{
+    public class Survey
+    {
+        [Column("SurveyId")]
+        public Guid Id { get; set; }
+
+
+        [Required(ErrorMessage = "Survey name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Title is 60 characters.")]
+        public string? Title { get; set; }
+        public string? Topic { get; set; }
+        public ICollection<Question>? Questions { get; set; }
+    }
+}
